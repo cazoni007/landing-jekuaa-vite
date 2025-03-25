d275724372bf8ee9c669b6a9dc1a7e7f254c5f07
cazoni007 <sebastiancazon301@gmail.com>
2025-03-25 15:54:54 -0400

    start to use hexagon to encapsulate imgs

--- src/Services/index.jsx
+++ src/Services/index.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { LandingContext } from '../LandingContext';
 import { NavLink } from 'react-router-dom';
+import { YellowHexagon } from "../IconSelector/YellowHexagon";
+import { CyanHexagon } from "../IconSelector/CyanHexagon";
+import { PurpleHexagon } from "../IconSelector/PurpleHexagon";
+import { LightOrangeHexagon } from "../IconSelector/LightOrangeHexagon";
+import { DarkOrangeHexagon } from "../IconSelector/DarkOrangeHexagon";
 import './Services.css'
 
 function Services() {
-    const {propuestas, activeSection, setActiveSection, activeSection2, setActiveSection2} = React.useContext(LandingContext);
+    const { propuestas, activeSection, setActiveSection, activeSection2, setActiveSection2 } = React.useContext(LandingContext);
     return (
         <section className='services' id='services'>
             <h2 className='services__title'>Nuestra propuesta:<br />Conectar, Educar y Transformar</h2>
@@ -21,12 +26,15 @@ function Services() {
 
                         {/* Lado trasero con imagen */}
                         <div className='services__back'>
-                            <img src={service.image} alt={`Imagen de ${service.title}`} className="services__image" />
+                            <YellowHexagon />
+                            <div className='hexagon'>
+                                <img src={service.image} alt={`Imagen de ${service.title}`} className="services__image" />
+                            </div>
                         </div>
                     </article>
                 ))}
             </div>
-            <NavLink className={`services__button--a ${activeSection && activeSection2 === 'experiencias' ? 'nabBar__item--focus dropMenu__item--focus' : ''}`} to='/experiencias' onClick={() => {setActiveSection('experiencias'); setActiveSection2('experiencias')}}><button className='services__button'>Nuestras experiencias diseñadas para ti</button></NavLink>
+            <NavLink className={`services__button--a ${activeSection && activeSection2 === 'experiencias' ? 'nabBar__item--focus dropMenu__item--focus' : ''}`} to='/experiencias' onClick={() => { setActiveSection('experiencias'); setActiveSection2('experiencias') }}><button className='services__button'>Nuestras experiencias diseñadas para ti</button></NavLink>
         </section>
     )
 }
