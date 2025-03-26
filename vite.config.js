ac60b83576981ffe318988560b758e2b06525ee8
cazoni007 <sebastiancazon301@gmail.com>
2025-03-26 17:10:19 -0400

    removing allowed host from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import svgr from 'vite-plugin-svgr'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(),],
-  server: {
-    allowedHosts: ["86c3-189-28-69-243.ngrok-free.app", "05cb-189-28-71-196.ngrok-free.app", "00cb-189-28-71-196.ngrok-free.app"]
-  }
 })
